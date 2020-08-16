f91cc012d3ba8f817613886dcc4483960d46613e
Cláudio Silva <claudio.engdist@gmail.com>
2020-08-16 16:18:49 -0300

    Update server.ts
    Get server port from environment variable

--- src/server.ts
+++ src/server.ts
@@ -8,4 +8,6 @@ app.use(cors());
 app.use(express.json());
 app.use(Routes);
 
-app.listen(3333);
+app.listen(process.env.PORT, () => {
+  console.log(`server started on port ${process.env.PORT}`)
+});
